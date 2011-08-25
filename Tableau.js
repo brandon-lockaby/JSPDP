e02a97a06d3e60b1af30936d847a2ad25bbf2639
Brandon Lockaby <gbrandon@gmail.com>
2011-08-25 06:35:35 -0400

    GPL license I guess

--- Tableau.js
+++ Tableau.js
@@ -1,4 +1,24 @@
 /*
+	Copyright 2011 Brandon Lockaby
+	
+	This file is part of JSPDP.
+
+    JSPDP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JSPDP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JSPDP.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/*
 	The playing field.
 	No behavior shall be defined in this class that should be specific to
 	any individual gameplay mode, or graphics or audio theme.
--- TouchController.js
+++ TouchController.js
@@ -1,3 +1,23 @@
+/*
+	Copyright 2011 Brandon Lockaby
+	
+	This file is part of JSPDP.
+
+    JSPDP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JSPDP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JSPDP.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 JSPDP.TouchController = function(tableau, element, panel_width, panel_height) {
 	this.tableau = tableau;
 	this.element = element;

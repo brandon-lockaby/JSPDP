f49f2a71ff811da2af1d345136c79c05dfba163c
Brandon Lockaby <gbrandon@gmail.com>
2011-11-07 01:15:54 -0500

    implement Function.prototype.bind for Safari and Opera...

--- JSPDP.js
+++ JSPDP.js
@@ -20,3 +20,12 @@
 */
 
 function JSPDP() {};
+
+if(!Function.prototype.bind) {
+	Function.prototype.bind = function() {
+		var fn = this, args = Array.prototype.slice.call(arguments), object = args.shift();
+		return function() {
+			return fn.apply(object, args.concat(Array.prototype.slice.call(arguments)));
+		};
+	};
+}
